d2deac1181b8a0409fc30b8b7077c0f306d3a11c
tongao <tongao713@live.cn>
2018-07-17 15:36:34 +0800

    jwt 发放和校验

---
+++ src/main/java/test/JwtTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import io.jsonwebtoken.Claims;
+import org.junit.Test;
+import util.JwtUtil;
+
+public class JwtTest {
+
+    @Test
+    public void testJwt() throws Exception {
+        JwtUtil util = new JwtUtil();
+        String ab = util.createJWT("jwt", "{id:100,name:xiaohong}", 6000000);
+        System.out.println(ab);
+        //eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiJEU1NGQVdEV0FEQVMuLi4iLCJzdWIiOiJ7aWQ6MTAwLG5hbWU6eGlhb2hvbmd9IiwidXNlcl9uYW1lIjoiYWRtaW4iLCJuaWNrX25hbWUiOiJEQVNEQTEyMSIsImV4cCI6MTUxNzgzNTE0NiwiaWF0IjoxNTE3ODM1MDg2LCJqdGkiOiJqd3QifQ.ncVrqdXeiCfrB9v6BulDRWUDDdROB7f-_Hg5N0po980
+        String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiJEU1NGQVdEV0FEQVMuLi4iLCJzdWIiOiJ7aWQ6MTAwLG5hbWU6eGlhb2hvbmd9IiwidXNlcl9uYW1lIjoiYWRtaW4iLCJuaWNrX25hbWUiOiJEQVNEQTEyMSIsImV4cCI6MTUzMTgxMjk3NiwiaWF0IjoxNTMxODEyOTE2LCJqdGkiOiJqd3QifQ.FWGrH3yvifPS3hpR7QIJvP-cQZg_nszFMc6b5VAV_Oc";
+        Claims c = util.parseJWT(jwt);//注意：如果jwt已经过期了，这里会抛出jwt过期异常。
+        System.out.println(c.getId());//jwt
+        System.out.println(c.getIssuedAt());//Mon Feb 05 20:50:49 CST 2018
+        System.out.println(c.getSubject());//{id:100,name:xiaohong}
+        System.out.println(c.getIssuer());//null
+        System.out.println(c.get("uid", String.class));//DSSFAWDWADAS...
+    }
+
+
+}
